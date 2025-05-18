8241e7d7adeb950a454a1eab5384c866c96495e0
Adán Romero <adanromeromarrero@gmail.com>
2025-05-18 15:12:10 +0200

    Creación de LibroRepository.

---
+++ src/test/java/com/Libros_Autores/Libros_Autores/LibrosAutoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Libros_Autores.Libros_Autores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrosAutoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
